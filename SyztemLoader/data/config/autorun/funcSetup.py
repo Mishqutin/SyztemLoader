23b44297fa25692aade7f20e263702234eb9c85f
Mishqutin <mundzioch@wp.pl>
2017-01-26 21:52:55 +0100

    Fixed print SCmd

--- SyztemLoader/data/config/autorun/funcSetup.py
+++ SyztemLoader/data/config/autorun/funcSetup.py
@@ -2,7 +2,7 @@
 # If you want use commands with arguments simply put args[int] in place.
 
 # SCmd
-sz.SZData("print", "SCmd", "print(args[0])")
+sz.SZData("print", "SCmd", "print(lEval(args[0]))")
 sz.SZData("getSZdata", "SCmd", "print(sz.SZdata)")
 sz.SZData("termColor", "SCmd", "os.system('color {}{}'.format(args[0], args[1]))")
 sz.SZData("terminate", "SCmd", "exit()")
