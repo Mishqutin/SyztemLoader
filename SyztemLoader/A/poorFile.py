3058223a0a43c916e6742c754fa4eda0b9307ba7
Mishqutin <mundzioch@wp.pl>
2017-02-11 22:52:15 +0100

    Added some text in scroller

--- SyztemLoader/A/poorFile.py
+++ SyztemLoader/A/poorFile.py
@@ -46,6 +46,11 @@ while True:
             ISC.do("output {}".format(x))
             ISC.do(x)
     elif mode == 'scroll':
+        editSidebar(0, 'Scrolling')
+        editSidebar(1, 'mode.')
+        editSidebar(2, 'w - up')
+        editSidebar(3, 's - down')
+        editSidebar(4, 'q - exit')
         x = msvcrt.getch()
         if x == b'w':
             scrollUp()
@@ -53,3 +58,8 @@ while True:
             scrollDown()
         elif x == b'q':
             sz.SZdata['termMode']['value'] = 'console'
+            editSidebar(0, '')
+            editSidebar(1, '')
+            editSidebar(2, '')
+            editSidebar(3, '')
+            editSidebar(4, '')
