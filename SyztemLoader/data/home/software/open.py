c5b8627324fd49657fde35b12a8fb30fc2af3581
Mishqutin <mundzioch@wp.pl>
2017-03-25 20:44:03 +0100

    Shortcuts are now saved in docs

--- SyztemLoader/data/home/software/open.py
+++ SyztemLoader/data/home/software/open.py
@@ -1,5 +1,5 @@
 if p[1]:
-    f = open(sz.config['sDir']+'\\data\\home\\user\\shortcuts\\'+p[1], 'r')
+    f = open(os.getenv('userprofile')+'\\documents\\Syztem\\shortcuts\\'+p[1], 'r')
     a = f.read()
     f.close()
     os.system('start {}'.format(a))
--- SyztemLoader/data/home/software/saveDir.py
+++ SyztemLoader/data/home/software/saveDir.py
@@ -1,4 +1,4 @@
 if p[1] and p[2]:
-    f = open(sz.config['sDir']+'\\data\\home\\user\\shortcuts\\'+p[2], 'w')
+    f = open(os.getenv('userprofile')+'\\documents\\Syztem\\shortcuts\\'+p[2], 'w')
     f.write(p[1])
     f.close()
